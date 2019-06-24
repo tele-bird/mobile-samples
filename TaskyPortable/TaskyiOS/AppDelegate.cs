1fb50d456778a80ba7b628fc6251d872dcbdb24a
Philip Hadley <Philip.Hadley@gmcr.com>
2019-06-24 16:39:58 -0400

    workaround for custom deep link URL issue on iOS

--- TaskyPortable/TaskyiOS/AppDelegate.cs
+++ TaskyPortable/TaskyiOS/AppDelegate.cs
@@ -6,9 +6,11 @@ using UIKit;
 using SQLite;
 using Tasky.PortableLibrary;
 using System.IO;
-using Firebase.DynamicLinks;
+//using Firebase.DynamicLinks;
 using ObjCRuntime;
 using System.Text;
+using System.Threading.Tasks;
+//using Firebase.Core;
 
 namespace Tasky 
 {
@@ -34,6 +36,8 @@ namespace Tasky
 		public static AppDelegate Current { get; private set; }
 		public TodoItemManager TodoManager { get; set; }
 		SQLiteConnection conn;
+
+        NSUrl _deepLinkUrl = null;
 
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
@@ -68,11 +72,84 @@ namespace Tasky
 			window.RootViewController = navController;
 			window.MakeKeyAndVisible ();
 
-            Firebase.Core.App.Configure();
-            DynamicLinks.PerformDiagnostics(null);
-			
-			return true;
+            //Options.DefaultInstance.DeepLinkUrlScheme = "com.keurig.app";
+            //App.Configure();
+            //DynamicLinks.PerformDiagnostics(null);
+
+            Console.WriteLine("Phil's FinishedLaunching is about to parse a deep link URL from the clipboard...");
+            _deepLinkUrl = GetDeepLinkUrlFromClipboard().Result;
+            if (_deepLinkUrl != null)
+            {
+                Console.WriteLine($"Phil's FinishedLaunching has parsed a deep link URL from the clipboard: {_deepLinkUrl}");
+                Console.WriteLine($"Phil's FinishedLaunching Clipboard contents after parsing: {GetClipboardText().Result}");
+                ShowMessage("FinishedLaunching found a deep link", _deepLinkUrl.ToString(), app.KeyWindow.RootViewController);
+            }
+
+
+            return true;
 		}
+
+        private void ShowMessage(string title, string message, UIViewController fromViewController)
+        {
+            var alert = UIAlertController.Create(title, message, UIAlertControllerStyle.Alert);
+            alert.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, (obj) => { }));
+            fromViewController.PresentViewController(alert, true, null);
+        }
+
+        private async Task<NSUrl> GetDeepLinkUrlFromClipboard()
+        {
+            NSUrl deepLinkUrl = null;
+            string clipboardText = await GetClipboardText(false);
+            Console.WriteLine($"Phil's GetDeepLinkUrlFromClipboard found on clipboard: {clipboardText}");
+            NSUrl firebaseUrl = null;
+            try
+            {
+                firebaseUrl = NSUrl.FromString(clipboardText);
+                Console.WriteLine($"Phil's GetDeepLinkUrlFromClipboard parsed a Firebase URL from clipboard: {firebaseUrl}");
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine($"Phil's GetDeepLinkUrlFromClipboard caught an {exc.GetType().FullName} while parsing the clipboard contents into a URL. clipboardText: {clipboardText}");
+            }
+            if (firebaseUrl != null && TryParseDeepLink(firebaseUrl, out deepLinkUrl))
+            {
+                await GetClipboardText(true);
+            }
+            return deepLinkUrl;
+        }
+
+        private bool TryParseDeepLink(NSUrl firebaseUrl, out NSUrl deepLinkUrl)
+        {
+            deepLinkUrl = null;
+            bool success = false;
+
+            NSUrlComponents urlComponents = new NSUrlComponents(firebaseUrl, false);
+            if(urlComponents.QueryItems != null)
+            {
+                NSUrl candidateDeepLinkUrl = null;
+                Dictionary<string, string> queryItems = new Dictionary<string, string>();
+                foreach (NSUrlQueryItem queryItem in urlComponents.QueryItems)
+                {
+                    queryItems.Add(queryItem.Name.ToLower(), queryItem.Value);
+                    if (queryItem.Name.ToLower() == "link")
+                    {
+                        candidateDeepLinkUrl = NSUrl.FromString(queryItem.Value);
+                    }
+                }
+
+                success =
+                    queryItems.Keys.Contains("isi") &&
+                    queryItems.Keys.Contains("ibi") &&
+                    candidateDeepLinkUrl != null;
+
+                if (success)
+                {
+                    deepLinkUrl = candidateDeepLinkUrl;
+                }
+            }
+
+            return success;
+        }
 
         public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
         {
@@ -81,67 +158,94 @@ namespace Tasky
 
         public override bool OpenUrl(UIApplication application, NSUrl url, string sourceApplication, NSObject annotation)
         {
-            Console.WriteLine($"I have received a URL through a custom scheme! {url.AbsoluteString}");
-            var dynamicLink = DynamicLinks.SharedInstance?.FromCustomSchemeUrl(url);
-            if(dynamicLink != null)
-            {
-                HandleIncomingDynamicLink(dynamicLink);
-                return true;
-            }
+            Console.WriteLine($"Phil's I have received a URL through a custom scheme! {url.AbsoluteString}");
+            ////var dynamicLink = DynamicLinks.SharedInstance?.FromCustomSchemeUrl(url);
+            ////if (dynamicLink != null)
+            ////{
+            ////    HandleIncomingDynamicLink(dynamicLink);
+            ////    return true;
+            ////}
 
             return false;
+        }
+
+        private async Task<string> GetClipboardText(bool cut = false)
+        {
+            string clipboardText = null;
+            if (Xamarin.Essentials.Clipboard.HasText)
+            {
+                string maybeEmptyClipboardText = await Xamarin.Essentials.Clipboard.GetTextAsync();
+                maybeEmptyClipboardText = maybeEmptyClipboardText.Trim();
+                if(!string.IsNullOrEmpty(maybeEmptyClipboardText))
+                {
+                    clipboardText = maybeEmptyClipboardText;
+                    if (cut)
+                    {
+                        await Xamarin.Essentials.Clipboard.SetTextAsync(string.Empty);
+                    }
+                }
+            }
+            return clipboardText;
         }
 
         public override bool ContinueUserActivity(UIApplication application, NSUserActivity userActivity, UIApplicationRestorationHandler completionHandler)
         {
             if(userActivity.WebPageUrl != null)
             {
-                Console.WriteLine($"Incoming URL is {userActivity.WebPageUrl}");
-                bool linkHandled = DynamicLinks.SharedInstance.HandleUniversalLink(userActivity.WebPageUrl, (dynamicLink, error) =>
+                bool linkHandled = false;
+                Console.WriteLine($"Phil's ContinueUserActivity Incoming URL is {userActivity.WebPageUrl}");
+                NSUrl deepLinkUrl = null;
+                if(TryParseDeepLink(userActivity.WebPageUrl, out deepLinkUrl))
                 {
-                    if (error != null)
-                    {
-                        Console.WriteLine($"Found an error! {error.LocalizedDescription}");
-                    }
-                    else if (dynamicLink != null)
-                    {
-                        Console.WriteLine(dynamicLink.Url?.AbsoluteString);
-                        HandleIncomingDynamicLink(dynamicLink);
-                    }
-                });
+                    Console.WriteLine($"Phil's ContinueUserActivity has parsed a deep link URL from the incoming URL: {deepLinkUrl}");
+                    linkHandled = true;
+                    ShowMessage("ContinueUserActivity found a deep link", deepLinkUrl.ToString(), application.KeyWindow.RootViewController);
+                }
+                ////bool linkHandled = DynamicLinks.SharedInstance.HandleUniversalLink(userActivity.WebPageUrl, (dynamicLink, error) =>
+                ////{
+                ////    if (error != null)
+                ////    {
+                ////        Console.WriteLine($"Found an error! {error.LocalizedDescription}");
+                ////    }
+                ////    else if (dynamicLink != null)
+                ////    {
+                ////        Console.WriteLine(dynamicLink.Url?.AbsoluteString);
+                ////        HandleIncomingDynamicLink(dynamicLink);
+                ////    }
+                ////});
 
                 if (linkHandled)
                 {
-                    Console.WriteLine("Link handled");
+                    Console.WriteLine("Phil's Link handled");
                 }
                 else
                 {
-                    Console.WriteLine("Link not handled");
+                    Console.WriteLine("Phil's Link not handled");
                 }
             }
 
             return false;
         }
 
-        private void HandleIncomingDynamicLink(DynamicLink dynamicLink)
-        {
-            if(dynamicLink.Url == null)
-            {
-                Console.WriteLine("That's weird.  My dynamic link object has no URL.");
-            }
-            else
-            {
-                Console.WriteLine($"Your incoming link parameter is {dynamicLink.Url.AbsoluteString}");
-                var components = new NSUrlComponents(dynamicLink.Url, resolveAgainstBaseUrl: false);
-                if (components.QueryItems != null)
-                {
-                    foreach(var queryItem in components.QueryItems)
-                    {
-                        Console.WriteLine($"Parameter {queryItem.Name} has a value of {queryItem.Value}");
-                    }
-                    Console.WriteLine($"Dynamic link match type is {dynamicLink.MatchType}");
-                }
-            }
-        }
+        ////private void HandleIncomingDynamicLink(DynamicLink dynamicLink)
+        ////{
+        ////    if(dynamicLink.Url == null)
+        ////    {
+        ////        Console.WriteLine("That's weird.  My dynamic link object has no URL.");
+        ////    }
+        ////    else
+        ////    {
+        ////        Console.WriteLine($"Your incoming link parameter is {dynamicLink.Url.AbsoluteString}");
+        ////        var components = new NSUrlComponents(dynamicLink.Url, resolveAgainstBaseUrl: false);
+        ////        if (components.QueryItems != null)
+        ////        {
+        ////            foreach(var queryItem in components.QueryItems)
+        ////            {
+        ////                Console.WriteLine($"Parameter {queryItem.Name} has a value of {queryItem.Value}");
+        ////            }
+        ////            Console.WriteLine($"Dynamic link match type is {dynamicLink.MatchType}");
+        ////        }
+        ////    }
+        ////}
     }
 }
